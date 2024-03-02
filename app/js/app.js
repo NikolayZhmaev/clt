6e65b0793c3274ff902d00c15f5507d5d7d8013d
Nikolay Zhmaev <nikolay.zhmaev85@gmail.com>
2024-03-02 16:49:10 +0600

    Setteings:
    -Swiper
    -Jquerry

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,14 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
+/*jshint multistr: true*/
+
+/*jshint esversion: 6*/
+
+import Swiper from 'swiper';
+import { Parallax, Mousewheel, Controller, Pagination } from 'swiper/modules';
+Swiper.use([Parallax, Mousewheel, Controller, Pagination]);
+
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
